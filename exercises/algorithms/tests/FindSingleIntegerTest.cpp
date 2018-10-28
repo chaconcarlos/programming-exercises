672791ebe1edeb8c6275018f4b97abd5bd398e94
Carlos Chacon <chacon.carlosj@gmail.com>
2018-10-28 12:55:48 -0500

    Added Floyd cycle detection test.

--- exercises/algorithms/tests/FindSingleIntegerTest.cpp
+++ exercises/algorithms/tests/FindSingleIntegerTest.cpp
@@ -22,7 +22,7 @@ using namespace testing;
 /* STATIC FUNCTIONS **********************************************************/
 
 /**
- * @brief Get the integer that is single in an array of non-zero integer duplicates.
+ * @brief Get the integer that is single in an array of integer duplicates.
  *
  * @param input The input array.
  *
@@ -41,9 +41,16 @@ findSingle(const std::vector<int>& input)
 
 /* IMPLEMENTATION ************************************************************/
 
-TEST(FindSingleInteger, single_is_1)
+TEST(FindSingleInteger, no_zero_single_is_1)
 {
   const std::vector<int> input = { 1, 2, 2, 3, 4, 5, 4, 3, 5 };
+
+  ASSERT_THAT(findSingle(input), Eq(1));
+}
+
+TEST(FindSingleInteger, includes_zero_single_is_1)
+{
+  const std::vector<int> input = { 0, 0, 2, 2, 3, 4, 5, 4, 3, 5, 1 };
 
   ASSERT_THAT(findSingle(input), Eq(1));
 }
---
+++ exercises/algorithms/tests/FloydCycleDetectionTest.cpp
@@ -0,0 +1,140 @@
+/**
+ * @file FloydCycleDetectionTest.cpp
+ *
+ * @brief Implementation of Floyd's algorithm for cycle detection on linked lists.
+ *
+ * @author   Carlos Chacon <chacon.carlosj@gmail.com>
+ * @date     Mar 21, 2018
+ */
+
+/* INCLUDES ******************************************************************/
+
+#include <gmock/gmock.h>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+
+
+#include <iostream>
+
+/* USINGS ********************************************************************/
+
+using namespace testing;
+
+/* DECLARATIONS **************************************************************/
+
+/**
+ * @brief Structure that represents a node in a linked list.
+ */
+struct ListNode
+{
+public:
+
+  /**
+   * @brief Initializes an instance of the ListNode struct.
+   *
+   * @param x The value of the node.
+   */
+  ListNode(int x)
+  : val(x)
+  , next(nullptr)
+  {
+  }
+
+public:
+
+  int val;
+  ListNode *next;
+};
+
+
+/* STATIC FUNCTIONS **********************************************************/
+
+/**
+ * @brief Pushes a new value to a linked list.
+ *
+ * @param [out] head The head node.
+ * @param [in]  x    The value.
+ */
+void push(ListNode** head, int x)
+{
+  ListNode* new_node = new ListNode(x);
+
+  new_node->next = *head;
+
+  *head = new_node;
+}
+
+/**
+ * @brief Detects a cycle in a linked list, and returns the node where the cycle begins.
+ *
+ * @param head The head of the linked list.
+ *
+ * @return If a cycle exists, returns the node where the cycle begins. If there are no cycles, returns NULL.
+ */
+static ListNode*
+getCycleNode(ListNode* const head)
+{
+  ListNode* slow      = head;
+  ListNode* fast      = head;
+  ListNode* cycleNode = nullptr;
+
+  while (slow != nullptr && fast != nullptr && fast->next != nullptr)
+  {
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if (slow == fast)
+    {
+      cycleNode = fast;
+      break;
+    }
+  }
+
+  fast = head;
+
+  while (cycleNode != nullptr && fast != slow)
+  {
+    fast = fast->next;
+    slow = slow->next;
+
+    if (fast == slow)
+      cycleNode = fast;
+  }
+
+  return cycleNode;
+}
+
+/* IMPLEMENTATION ************************************************************/
+
+TEST(DetectCycle, floyd_cycle_exists_detected)
+{
+  ListNode* head = new ListNode(1);
+
+  push(&head, 20);
+  push(&head, 4);
+  push(&head, 15);
+  push(&head, 10);
+
+  head->next->next->next->next = head;
+
+  const ListNode* const cycleNode = getCycleNode(head);
+
+  ASSERT_THAT(cycleNode, Eq(head));
+}
+
+TEST(DetectCycle, floyd_no_cycle_exists_null)
+{
+  ListNode* head = new ListNode(1);
+
+  push(&head, 20);
+  push(&head, 4);
+  push(&head, 15);
+  push(&head, 10);
+
+  const ListNode* const cycleNode = getCycleNode(head);
+
+  ASSERT_THAT(cycleNode, Eq(nullptr));
+}
